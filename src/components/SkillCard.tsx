595e817d32a37ae24260a22f7c13426b59b8cce3
Kasun <kasun.ranasinghe@icloud.com>
2024-02-17 00:22:07 +0530

    completed skill section

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import styles from '../styles/SkillCard.module.css'
+
+interface SkillCardProps {
+  title: string;
+  imageUrl: string;
+}
+
+export default function SkillCard({ title, imageUrl }: SkillCardProps) {
+  const [style, setStyle] = useState<React.CSSProperties>({});
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { left, top, width, height } = e.currentTarget.getBoundingClientRect();
+    const centerX = left + width / 2;
+    const centerY = top + height / 2;
+    const percentX = (e.clientX - centerX) / (width / 2);
+    const percentY = (e.clientY - centerY) / (height / 2);
+    const tiltMaxAngle = 4; // Max tilt angle in degrees
+
+    setStyle({
+      transform: `rotateY(${percentX * tiltMaxAngle}deg) rotateX(${percentY * -tiltMaxAngle}deg)`,
+      transition: 'transform 0.1s',
+    });
+  };
+
+  const resetTilt = () => {
+    setStyle({
+      transform: 'rotateY(0deg) rotateX(0deg)',
+      transition: 'transform 0.5s',
+    });
+  };
+
+  return (
+    <div className={styles.skillCard}
+      style={style}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setStyle({ transition: 'none' })} // Remove transition for instant effect on enter
+      onMouseLeave={resetTilt}>
+      <img src={imageUrl} alt={title} />
+    </div>
+  );
+}
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,37 +1,71 @@
 'use client';
 import styles from "../styles/Skills.module.css"
+import SkillCard from "./SkillCard"
+
+const skills = [
+  {
+    title: 'Rust',
+    description: 'Blazingly fast and memory-efficient prgramming language',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/rust/rust-original.svg'
+  },
+  {
+    title: 'Actix-web',
+    description: 'Balingly fast web framework for Rust',
+    imageUrl: 'https://avatars.githubusercontent.com/u/32776943?s=200&v=4'
+  },
+  {
+    title: 'Tokio',
+    description: 'A runtime for writing reliable asynchronous applications with Rust',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Tokio_logo.svg/533px-Tokio_logo.svg.png'
+  },
+  {
+    title: 'Java',
+    description: 'Best programming language for enterprise applications',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg'
+  },
+  {
+    title: 'Spring Boot',
+    description: 'Enterprise grade framework for Java',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg'
+  },
+  {
+    title: 'MySQL',
+    description: 'The world\'s most popular open source database',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg'
+  },
+  {
+    title: 'OpenDJ',
+    description: 'Open source LDAP directory services',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/OpenDJ_Logo.svg/605px-OpenDJ_Logo.svg.png'
+  },
+  {
+    title: 'Kafka',
+    description: 'Distributed streaming platform',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/apachekafka/apachekafka-original.svg'
+  },
+  {
+    title: 'AWS',
+    description: 'Amazon Web Services',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg'
+  },
+  {
+    title: 'React',
+    description: 'Frontend library for building user interfaces',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg'
+  },
+  {
+    title: 'NeoVim',
+    description: 'Best IDE for developers',
+    imageUrl: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/neovim/neovim-original.svg'
+  },
+];
 
 export default function Skills() {
   return (
-    <section className={styles.skillsSection}>
-      <div className={styles.container}>
-        <h2 className={styles.heading}>Skills</h2>
-
-        {/* Technical Skills */}
-        <div className={styles.skillsBlock}>
-          <h3 className={styles.subheading}>Technical Skills</h3>
-          <ul className={styles.skillsList}>
-            <li>Rust</li>
-            <li>Java</li>
-            <li>Spring</li>
-            <li>Kafka</li>
-            <li>Docker</li>
-            <li>Kubernetes</li>
-          </ul>
-        </div>
-
-        {/* Soft Skills */}
-        <div className={styles.skillsBlock}>
-          <h3 className={styles.subheading}>Soft Skills</h3>
-          <ul className={styles.skillsList}>
-            <li>Effective Communication</li>
-            <li>Problem Solving</li>
-            <li>Teamwork</li>
-            <li>Adaptability</li>
-            <li>Creativity</li>
-          </ul>
-        </div>
-      </div>
-    </section>
+    <div className={styles.skillsSection}>
+      {skills.map(skill => (
+        <SkillCard key={skill.title} title={skill.title} imageUrl={skill.imageUrl} />
+      ))}
+    </div>
   );
 }
