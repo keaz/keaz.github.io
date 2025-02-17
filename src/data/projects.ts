249765806fce7793956dd6c189b33dc2c3445c75
Kasun <kasun.ranasinghe@icloud.com>
2025-02-17 21:42:16 +0530

    Added new projects
    Signed-off-by: Kasun <kasun.ranasinghe@icloud.com>

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,45 +1,61 @@
 const projects: Project[] = [
-  {
-    id: 1,
-    slug: 'term-ai',
-    title: 'Term AI',
-    description: 'AI terminal assistant for developers',
-    imageUrl: '/path-to-image-1.jpg', // Place your project image in the public folder
-    detailPageUrl: 'https://github.com/keaz/term-ai' // Hypothetical URL to a detailed view
-  },
-  {
-    id: 2,
-    slug: 'rchess',
-    title: 'Rust Chess',
-    description: 'A chess engine written in Rust',
-    imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
-    detailPageUrl: 'https://github.com/keaz/rchess' // Hypothetical URL to a detailed view
-  },
-  {
-    id: 3,
-    slug: 'decentralized-peer',
-    title: 'Decentralized Peer',
-    description: 'Decentralized peer to peer file sharing system',
-    imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
-    detailPageUrl: 'https://github.com/keaz/decentralized-peer' // Hypothetical URL to a detailed view
-  },
-  {
-    id: 4,
-    slug: 'rendezvous-server',
-    title: 'RenDezvous Server',
-    description: 'A Rendezvous server for decentralize file sharing system, written in Rust',
-    imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
-    detailPageUrl: 'https://github.com/keaz/rendezvous-server' // Hypothetical URL to a detailed view
-  },
-  {
-    id: 5,
-    slug: 'yt-as-storage',
-    title: 'Youutube as Storage',
-    description: 'A tool to store files in youtube videos, written in Rust',
-    imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
-    detailPageUrl: 'https://github.com/keaz/yt-as-storage' // Hypothetical URL to a detailed view
-  },
-  // Add more projects as needed
+    {
+        id: 1,
+        slug: 'simple-ldap',
+        title: 'Simple Ldap',
+        description: 'A easy to use Rust LDAP library with high-level capabilities',
+        imageUrl: '/path-to-image-1.jpg', // Place your project image in the public folder
+        detailPageUrl: 'https://github.com/keaz/simple-ldap' // Hypothetical URL to a detailed view
+    },
+    {
+        id: 2,
+        slug: 'high-level-kafka',
+        title: 'High Level Kafka',
+        description: 'A high-level Rust Kafka client',
+        imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
+        detailPageUrl: 'https://github.com/keaz/high-level-kafka' // Hypothetical URL to a detailed view
+    },
+    {
+        id: 3,
+        slug: 'decentralized-peer',
+        title: 'Decentralized Peer',
+        description: 'Decentralized peer to peer file sharing system',
+        imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
+        detailPageUrl: 'https://github.com/keaz/decentralized-peer' // Hypothetical URL to a detailed view
+    },
+    {
+        id: 4,
+        slug: 'rendezvous-server',
+        title: 'RenDezvous Server',
+        description: 'A Rendezvous server for decentralize file sharing system, written in Rust',
+        imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
+        detailPageUrl: 'https://github.com/keaz/rendezvous-server' // Hypothetical URL to a detailed view
+    },
+    {
+        id: 5,
+        slug: 'kcli',
+        title: 'Kfcli',
+        description: 'A CLI tool to inspect Kafka clusters',
+        imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
+        detailPageUrl: 'https://github.com/keaz/kcli' // Hypothetical URL to a detailed view
+    },
+    {
+        id: 6,
+        slug: 'rust-copy',
+        title: 'Rust Copy',
+        description: 'A replacement for cp',
+        imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
+        detailPageUrl: 'https://github.com/keaz/rust-copy' // Hypothetical URL to a detailed view
+    },
+    {
+        id: 7,
+        slug: 'actix-web-jwt',
+        title: 'Actix Web Jwt',
+        description: 'JWT validation middleware for Actix-Web',
+        imageUrl: '/path-to-image-2.jpg', // Place your project image in the public folder
+        detailPageUrl: 'https://github.com/keaz/actix-web-jwt' // Hypothetical URL to a detailed view
+    },
+    // Add more projects as needed
 ];
 
 export default projects;
